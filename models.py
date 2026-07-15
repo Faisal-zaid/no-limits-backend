c83cc5fcab7e68cbbd5b6ae210594a294f91d4e6
faisal ali zaid <zaidalideveloper@gmail.com>
2026-07-15 15:10:43 +0300

    fixed calss orderitem

--- models.py
+++ models.py
@@ -82,7 +82,7 @@ class OrderItem(Base): #stores each product inside an order, e.g an order can ha
     __tablename__="order_items"
 
     id=Column(Integer, primary_key=True)
-    order_id=Column(Integer, ForeignKey("Orders.id"))
+    order_id=Column(Integer, ForeignKey("orders.id"))
     product_id=Column(Integer, ForeignKey("products.id"))
     quantity=Column(Integer)
     order=relationship("Order", back_populates="items")

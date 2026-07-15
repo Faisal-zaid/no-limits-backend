16a7473ec1d8937d7f4262d59eea2eeefcb5bf92
faisal ali zaid <zaidalideveloper@gmail.com>
2026-07-15 14:35:11 +0300

    added class order items

--- models.py
+++ models.py
@@ -77,3 +77,19 @@ class Order(Base):
         back_populates="order",
         cascade="all, delete-orphan"
     )
+
+class OrderItem(Base):
+    __tablename__="order_items"
+
+    id=Column(Integer, primary_key=True)
+    order_id=Column(Integer, ForeignKey("Orders.id"))
+    product_id=Column(Integer, ForeignKey("Products.id"))
+    quantity=Column(Integer)
+    order=relationship("Order", back_populates="items")
+    product=relationship("Product")
+
+    values=relationship(
+        "OrderItemFieldValue",
+        back_populates="order_item",
+        cascade="all, delete-orphan"
+    )    

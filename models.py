efa48d41f73a18a83b4e3aee727030870a79413a
faisal ali zaid <zaidalideveloper@gmail.com>
2026-07-15 14:56:53 +0300

    imported more

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 #import necessary packages
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column,Integer,Text,DateTime
+from sqlalchemy import Column,Integer,Text,DateTime,String,relationship,ForeignKey,Boolean
 
 #set up base class where models will inherit from
 Base=declarative_base()

a97096095aee9043749b2817197373c3b73a03aa
faisal ali zaid <zaidalideveloper@gmail.com>
2026-07-15 14:06:47 +0300

    added Category class

--- models.py
+++ models.py
@@ -4,3 +4,14 @@ from sqlalchemy import Column,Integer,Text,DateTime
 
 #set up base class where models will inherit from
 Base=declarative_base()
+
+#begining of modelling the clases
+class Category(Base):
+    __tablename__="categories"
+
+    id=Column(Integer, primary_key=True, index=True)
+    name=Column(String, nullable=False, unique=True)
+    description=Column(Text)
+    image=Column(String)
+
+    products=relationship("Product", back_populates="category")

83e353ebfd0a9bac02fa3f6450e16e2f663f8a60
faisal ali zaid <zaidalideveloper@gmail.com>
2026-07-16 14:36:51 +0300

    that line enables cors

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from fastapi import FastAPI, Depends # Depnds is added so records are persisted 
 from models import get_db,Category, Product, ProductField, ProductFieldOption, Order, OrderItem, OrderItemFieldValue 
 #we need to do data validation using pydantic
 from pydantic import BaseModel
+#this import allows enabling of CORS
+from fastapi.middleware.Cors import CORSMiddleware
 
 #create an instance
 app=FastAPI()

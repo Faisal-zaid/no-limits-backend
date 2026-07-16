4c2391aed35a17a51a9919941c9a244c6d1c624c
faisal ali zaid <zaidalideveloper@gmail.com>
2026-07-16 13:23:32 +0300

    added notes

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+#import fastapi class
 from fastapi import FastAPI
 
+#create an instance
 app=FastAPI()
 
+#create routes and access resources 
 @app.get("/")
 def read_root():
     return{"Hello":"world!"}

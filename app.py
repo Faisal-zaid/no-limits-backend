5418ae137f49cdb8367ffd7b49be1d1591fd017d
faisal ali zaid <zaidalideveloper@gmail.com>
2026-07-16 13:27:36 +0300

    added a route for creating a single category

--- app.py
+++ app.py
@@ -8,3 +8,10 @@ app=FastAPI()
 @app.get("/")
 def read_root():
     return{"Hello":"world!"}
+
+#create a single category 
+@app.post("/category")
+def create_category():
+    #this is where i will come to use sqlalchemy to create records
+
+    return{"message":"Category created successfully"}

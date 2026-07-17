960999e69eda277e672910a709c71146d3a5b5e6
faisal ali zaid <zaidalideveloper@gmail.com>
2026-07-17 10:46:51 +0300

    added import of category route

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from models import get_db,Category, Product, ProductField, ProductFieldOption, O
 from pydantic import BaseModel
 #this import allows enabling of CORS
 from fastapi.middleware.cors import CORSMiddleware
+#tells fastapi of the separate route that will communicate to it
+from routes.Category import router as category_router
 
 #create an instance
 app=FastAPI()

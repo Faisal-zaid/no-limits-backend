2b5ec96d28799874c3db9423a1f495d0a98895b9
faisal ali zaid <zaidalideveloper@gmail.com>
2026-08-06 15:09:32 +0300

    final and made changes to update categories

--- routes/Category.py
+++ routes/Category.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 import cloudinary.uploader
 import cloudinary_config
 from fastapi import UploadFile, File, Form
+from typing import Optional #adding this so i can use "|"  operand
 
 router=APIRouter()
 

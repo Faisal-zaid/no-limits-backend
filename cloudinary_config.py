1a0fcf1ded260234f2fdf0697788093c5c8df523
faisal ali zaid <zaidalideveloper@gmail.com>
2026-08-06 13:28:46 +0300

    final

--- cloudinary_config.py
+++ cloudinary_config.py
@@ -2,6 +2,8 @@ import cloudinary
 import os
 from dotenv import load_dotenv
 
+load_dotenv()
+
 cloudinary.config(
     cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
     api_key=os.getenv("CLOUDINARY_API_KEY"),

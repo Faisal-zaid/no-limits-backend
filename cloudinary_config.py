43b419abfb4fa143e623827345fa22710772a031
faisal ali zaid <zaidalideveloper@gmail.com>
2026-08-06 13:25:04 +0300

    added environment variables for security

--- cloudinary_config.py
+++ cloudinary_config.py
@@ -1,8 +1,8 @@
 import cloudinary
 
 cloudinary.config(
-    cloud_name="your cloud name",
-    api_key="your api key",
-    api_secret="your api secret",
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
     secure=True
 )

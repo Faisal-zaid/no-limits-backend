60755052f059f41b4a22c666a8981d1ac665d5d8
faisal ali zaid <zaidalideveloper@gmail.com>
2026-08-16 12:51:54 +0300

    allow headers in app.py cors

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ app.include_router(productfieldoption_router)
 app.include_router(checkout_router)
 
 #allow access from all servers
-app.add_middleware(CORSMiddleware,  allow_origins=["*"],allow_methods=["*"])
+app.add_middleware(CORSMiddleware,  allow_origins=["*"],allow_headers=["*"],allow_methods=["*"])
 
 #create routes and access resources 
 @app.get("/")

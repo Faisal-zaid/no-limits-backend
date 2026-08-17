ed71eb822c826b569d67d96201b83806e03b2660
faisal ali zaid <zaidalideveloper@gmail.com>
2026-08-17 10:38:31 +0300

    created the route for this route

--- routes/OrderItemFieldValue.py
+++ routes/OrderItemFieldValue.py
@@ -10,3 +10,28 @@ class OrderItemFieldValueSchema(BaseModel):
     order_item_id: int
     product_field_id: int
     value: str
+
+#create rooute
+
+@router.post("/orderitemfieldvalue")
+def create_order_item_field_value(
+    data: OrderItemFieldValueSchema,
+    session=Depends(get_db)
+):
+
+    new_value = OrderItemFieldValue(
+        order_item_id=data.order_item_id,
+        product_field_id=data.product_field_id,
+        value=data.value
+    )
+
+    session.add(new_value)
+
+    session.commit()
+
+    session.refresh(new_value)
+
+    return {
+        "message": "Order item field value created successfully",
+        "order_item_field_value_id": new_value.id
+    }

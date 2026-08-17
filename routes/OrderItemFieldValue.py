c55d21e672bf361fd002bd8dd966fea9c004fa9c
faisal ali zaid <zaidalideveloper@gmail.com>
2026-08-17 10:37:32 +0300

    added validation

--- routes/OrderItemFieldValue.py
+++ routes/OrderItemFieldValue.py
@@ -3,3 +3,10 @@ from models import OrderItemFieldValue, get_db
 from pydantic import BaseModel
 
 router = APIRouter()
+
+#validation
+
+class OrderItemFieldValueSchema(BaseModel):
+    order_item_id: int
+    product_field_id: int
+    value: str

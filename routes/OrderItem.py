28ec7088e34e388fa54a5393578eb88255e7ea1c
faisal ali zaid <zaidalideveloper@gmail.com>
2026-08-17 10:19:12 +0300

    added validation

--- routes/OrderItem.py
+++ routes/OrderItem.py
@@ -3,3 +3,10 @@ from models import OrderItem, get_db
 from pydantic import BaseModel
 
 router = APIRouter()
+
+#validation
+
+class OrderItemSchema(BaseModel):
+    order_id: int
+    product_id: int
+    quantity: int

44923bdd1a9bca808d3eaa2e0725d6f601dd1f27
faisal ali zaid <zaidalideveloper@gmail.com>
2026-09-07 23:42:28 +0300

    added imports in ratelimiter

--- rate_limit.py
+++ rate_limit.py
@@ -1,6 +1,7 @@
 from fastapi import Request, HTTPException, status, Depends
+from fastapi_limiter import FastAPILimiter
 from fastapi_limiter.depends import RateLimiter
-
+import redis.asyncio as redis   # they allow us to use ratelimiters
 
 
 # IDENTIFIER

6af2bfd56c1be0f5b1464d6bc81aa966df78836a
faisal ali zaid <zaidalideveloper@gmail.com>
2026-09-21 12:06:16 +0300

    changed import

--- seed_admin.py
+++ seed_admin.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from passlib.context import CryptContext
 from models import User, Session
-from database import engine, Base  # Adjust import paths to match your project
+from models import engine, Base  # Adjust import paths to match your project
 
 def create_admin():
     # Force creation of missing columns/tables if they don't exist
